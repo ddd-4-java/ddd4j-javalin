213b60623b797208848d2ee09f15097e2d7ea241
wandl-6A72h <hiwepy@gmail.com>
2026-07-07 23:34:56 +0800

    refactor(rabbitmq): 更新RabbitMQ模块的包路径引用
    - 将RabbitMQConsumerEndpointRegistrar的导入路径从rabbit包更改为rabbitmq包
    - 将RabbitBrokerAdapter的导入路径从rabbit包更改为rabbitmq包
    - 将AmqpAcknowledgmentFactory的导入路径从rabbit包更改为rabbitmq包
    - 统一RabbitMQ相关组件的包结构命名规范

--- ddd4j-javalin-mq/ddd4j-javalin-mq-rabbitmq/src/main/java/io/ddd4j/javalin/mq/rabbit/Ddd4jRabbitMqGuiceModule.java
+++ ddd4j-javalin-mq/ddd4j-javalin-mq-rabbitmq/src/main/java/io/ddd4j/javalin/mq/rabbit/Ddd4jRabbitMqGuiceModule.java
@@ -5,8 +5,8 @@ import com.google.inject.Singleton;
 import io.ddd4j.javalin.mq.core.AbstractDdd4jMqGuiceModule;
 import io.ddd4j.mq.config.Ddd4jMQProperties;
 import io.ddd4j.mq.event.MQEventPublisher;
-import io.ddd4j.mq.rabbit.RabbitMQBrokerAdapter;
-import io.ddd4j.mq.rabbit.RabbitMQProperties;
+import io.ddd4j.mq.rabbitmq.RabbitMQBrokerAdapter;
+import io.ddd4j.mq.rabbitmq.RabbitMQProperties;
 import io.ddd4j.mq.spi.MQBrokerAdapter;
 
 import java.util.Objects;
